124bec3d319edb78bfffc7e7ee31e3b641a00653
Luis Tavares <mikusher@hotmail.com>
2021-02-26 09:49:22 +0000

    commit env file

--- MicroservicesBackend/settings.py
+++ MicroservicesBackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7np!34!@ngp(z%@n46i9=(zjq5@ybra!3yli6j^yuqm@x7z82c'
+# create a .env file -> I will commit the .env but we know that I should not do! xD
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
